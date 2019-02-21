546032c60bef13b54f9015d5ab75a1cb01f53a3b
Mir Carvalho <deppbrazil@gmail.com>
2019-02-21 02:50:07 -0300

    :rocket: Add buttton and function translate en/pt-br, Update card cv, Update slogan, Update line spaces, Fix cv link, Update copyright

---
+++ dist/js/button.js
@@ -0,0 +1,7 @@
+function openBR() {
+    setTimeout(function(){ location.href="https://www.deppbrazil.com.br"; }, 1000);
+};
+
+function openUS() {
+    setTimeout(function(){ location.href="https://www.deppbrazil.com"; }, 1000);
+    };
